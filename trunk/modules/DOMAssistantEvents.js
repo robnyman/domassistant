9a925b15f0a7fa94a0f01d9fa76ed78d2ac82492
robnyman <robnyman@4dc669ec-2940-0410-9f04-cfe5fb82e8a9>
2007-12-29 23:07:28 +0000

    Rewritten event handling, for event delegation and better returns. addEventListener not used anymore

--- trunk/modules/DOMAssistantEvents.js
+++ trunk/modules/DOMAssistantEvents.js
@@ -3,7 +3,9 @@
 DOMAssistant.Events = function () {
 	var baseMethodsToAdd = [
 		"addEvent",
-		"removeEvent"
+		"removeEvent",
+		"preventDefault",
+		"cancelBubble"
 	];
 	var HTMLArrayEventMethods = {
 		addEvent : function (evt, func) {
@@ -15,6 +17,18 @@ DOMAssistant.Events = function () {
 		removeEvent : function (evt, func) {
 			for (var i=0, il=this.length; i<il; i++) {
 				this.Events.removeEvent.call(this[i], evt, func);
+			}
+			return this;
+		},
+		preventDefault : function (evt) {
+			for (var i=0, il=this.length; i<il; i++) {
+				this.Events.preventDefault.call(this[i], evt);
+			}
+			return this;
+		},
+		cancelBubble : function (evt) {
+			for (var i=0, il=this.length; i<il; i++) {
+				this.Events.cancelBubble.call(this[i], evt);
 			}
 			return this;
 		}
@@ -32,60 +46,48 @@ DOMAssistant.Events = function () {
 		},
 
 		addEvent : function (evt, func) {
-			if (this.addEventListener) {
-				DOMAssistant.Events.addEvent = function (evt, func) {
-					this.addEventListener(evt, func, false);
-					return this;
-				};
+			if (!this.events) {
+				this.events = {};
 			}
-			else{
-				DOMAssistant.Events.addEvent = function (evt, func) {
-					if (!this.events) {
-						this.events = {};
-					}
-					if (!this.events[evt]) {
-						this.events[evt] = [];
-					}							
-					this.events[evt].push(func);
-					this["on" + evt] = DOMAssistant.Events.handleEvent;
-					if (typeof this.window === "object") {
-						this.window["on" + evt] = DOMAssistant.Events.handleEvent;
-					}
-					return this;
-				};
+			if (!this.events[evt]) {
+				this.events[evt] = [];
+			}							
+			this.events[evt].push(func);
+			this["on" + evt] = DOMAssistant.Events.handleEvent;
+			if (typeof this.window === "object") {
+				this.window["on" + evt] = DOMAssistant.Events.handleEvent;
 			}
-			return DOMAssistant.Events.addEvent.call(this, evt, func);
+			return this;
 		},
 
 		handleEvent : function (evt) {
 			var currentEvt = evt || event;
+			var currentTarget = currentEvt.target || currentEvt.srcElement || document;
+			while (currentTarget.nodeType !== 1 && currentTarget.parentNode) {
+				currentTarget = currentTarget.parentNode;
+			}			
+			currentEvt.eventTarget = currentTarget;
 			var eventType = currentEvt.type;
 			var eventColl = this.events[eventType];
-			for (var i=0; i<eventColl.length; i++) {
-				eventColl[i].call(this, currentEvt);
+			var eventCollLength = eventColl.length;
+			var eventReturn;
+			for (var i=0; i<eventCollLength; i++) {
+				eventReturn = eventColl[i].call(this, currentEvt);
+				if (i === (eventCollLength - 1)) {
+					return eventReturn;
+				}
 			}
 		},
 
 		removeEvent : function (evt, func) {
-			if (this.removeEventListener) {
-				DOMAssistant.Events.removeEvent = function (evt, func) {
-					this.removeEventListener(evt, func, false);
-					return this;
-				};
+			var eventColl = this.events[evt];
+			for (var i=0; i<eventColl.length; i++) {
+				if (eventColl[i] === func) {
+					delete eventColl[i];
+					eventColl.splice(i, 1);
+				}
 			}
-			else if (this.events) {
-				DOMAssistant.Events.removeEvent = function (evt, func) {
-					var eventColl = this.events[evt];
-					for (var i=0; i<eventColl.length; i++) {
-						if (eventColl[i] === func) {
-							delete eventColl[i];
-							eventColl.splice(i, 1);
-						}
-					}
-					return this;
-				};
-			}
-			return DOMAssistant.Events.removeEvent.call(this, evt, func);
+			return this;
 		},
 
 		preventDefault : function (evt) {
