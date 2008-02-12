5e6b36b2aaf04ab7f0cf558f7ab463130224d2c4
robnyman <robnyman@4dc669ec-2940-0410-9f04-cfe5fb82e8a9>
2008-02-12 00:05:34 +0000

    Created favicon

--- domassistant-site/header-end.php
+++ domassistant-site/header-end.php
@@ -4,5 +4,5 @@
 	<!--[if lt IE 7]>
 		<link rel="stylesheet" href="/css/ie.css" type="text/css">
 	<![endif]-->
-	<link rel="Shortcut icon" href="http://www.robertnyman.com/favicon.png">
+	<link rel="Shortcut icon" href="/favicon.png">
 </head>
