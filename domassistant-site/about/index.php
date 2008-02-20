c8f7f3b55451fe85c3a35643c450a0427ea6fa08
robnyman <robnyman@4dc669ec-2940-0410-9f04-cfe5fb82e8a9>
2008-02-20 20:57:02 +0000

    Updated bios about the team

--- domassistant-site/about/index.php
+++ domassistant-site/about/index.php
@@ -28,21 +28,30 @@
 
 						<div id="main-content-area">
 							<h1>About</h1>
-							<p>DOMAssistant was originally developed by <a href="http://www.robertnyman.com/">Robert Nyman</a> to meet a need of a small yet very powerful JavaScript library, focusing on performance and modularity. Robert still develops the library, but now through the help of the invaluable DOMAssistant Team</p>
+							<p>DOMAssistant was originally developed by <a href="http://www.robertnyman.com/">Robert Nyman</a> to meet a need of a small yet very powerful JavaScript library, focusing on performance and modularity. Robert still develops the library, but now through the help of the invaluable DOMAssistant Team.</p>
 							
-							<!--
-							<h2>The DOMAssistant Team</h2>
+							<h2 id="domassistant-team">The DOMAssistant Team</h2>
 							<p>The DOMAssistant team consists of:</p>
-							<ul>
-								<li>Developer 1</li>
-							</ul>
-							-->
+							<dl>
+								<dt>Lim Cheng Hong</dt>
+								<dd>Cheng Hong currently resides and works in Singapore as a Senior	Consultant for a software firm. User interface is his passion, and he swears by accessible, usable and standards-based designs.</dd>
+								<dt><a href="http://kodfabrik.se/blog/">Pelle Wessman</a></dt>
+								<dd>Pelle is a freelancing Web Developer from Eslöv in Skåne, southern Sweden. He's working with Javascript, PHP, MySQL, HTML, CSS etc depending on the current assignment. He is a strong follower of standards and semantics and really likes initiatives like Microformats.</dd>
+								<dt><a href="http://www.456bereastreet.com/">Roger Johansson</a></dt>
+								<dd>Roger is working as a Front-end Web Developer at NetRelations in Göteborg, Sweden. He has a strong passion for web standards and accessibility, and have been blogging about those topics for almost five years now at <a href="http://www.456bereastreet.com/">456 Berea Street.</a></dd>
+								<dt><a href="http://www.oktavilla.se/oktavilla/gustaf_forsslund">Gustaf Forsslund (in Swedish)</a></dt>
+								<dd>Gustaf is based in Stockholm, Sweden, and works as an Interface and Web Developer at the 								Swedish design company Oktavilla (where he is also a partner and co-founder). He is passionate about modern web design practices like unobtrusiveness, semantics and graceful degradation, and has always been balancing on the sharp edge between client and server side programming, but is now definitely on the client side.</dd>
+								<dt>Carsten Witt</dt>
+								<dd>Carsten is living and working as a media designer (print) in Kiel, Northern Germany. He has a strong leaning towards technical things such as final drawing, passional, web conceptual design and development. Dreams in angle brackets.</dd>
+								<dt>Gustaf Lindqvist</dt>
+								<dd>Gustaf is living and working in Sundsvall, in northern Sweden, as a consultant System/Web Developer for a world leading end-to-end IT services provider.</dd>
+							</dl>
 							
-							<h2>Mailing list and discussion</h2>
+							<h2 id="mailing-list">Mailing list and discussion</h2>
 							<p>Send an e-mail to mailing-list [at] domassistant [dot] com with your question, or to verify your e-mail address and that you want to receive information about updates.</p>
 							<p>If you're interested in discussing features or getting support, please visit the <a href="http://groups.google.com/group/domassistant">DOMAssistant Discussion Group</a>.</p>
 							
-							<h2>DOMAssistant Compatability</h2>
+							<h2 id="compatibility">DOMAssistant Compatibility</h2>
 							<p>DOMAssistant has been tested and verified to work in these web browsers, but for most web browsers, it is fair to assume a long backwards compatibility:</p>
 							<ul>
 								<li>Internet Explorer 5.5+</li>
@@ -51,25 +60,18 @@
 								<li>Opera 9</li>
 							</ul>
 							
-							<h2>DOMAssistant Adaptions</h2>
+							<h2 id="adaptions">DOMAssistant Adaptions</h2>
 							<p>DOMAssistant utilizes XPath, querySelectorAll and native getElementsByClassName in web browsers that support it, for best performance. It is also optimized to avoid causing memory leaks in Internet Explorer.</p>
 						</div>
 
 						<div id="sidebar">
-							<h2>Download DOMAssistant</h2>
-							<dl>
-								<dt>
-									<a href="http://code.google.com/p/domassistant/downloads/detail?name=DOMAssistantCompressed-2.6.js">DOMAssistant 2.6, compressed</a><br>
-									(22kb, 7kb <a href="http://en.wikipedia.org/wiki/Gzip" class="small">Gzipped</a>)
-								</dt>
-								<dd>Intended for production usage.</dd>
-
-								<dt>
-									<a href="http://code.google.com/p/domassistant/downloads/detail?name=DOMAssistantComplete.2.6.js">DOMAssistant 2.6, complete</a><br>
-									(47kb)
-								</dt>
-								<dd>Intended for learning and development usage.</dd>
-							</dl>	
+							<h2>About DOMAssistant</h2>
+							<ul>
+								<li><a href="#domassistant-team">The DOMAssistant Team</li>
+								<li><a href="#mailing-list">Mailing list and discussion</a></li>
+								<li><a href="#compatibility">Compatibility</a></li>
+								<li><a href="#adaptions">Adaptions</a></li>
+							</ul>
 						</div>
 					</div>
 					<?php include "../get-your-copy.php" ?>
