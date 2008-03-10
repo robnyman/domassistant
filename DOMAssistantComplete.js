ce289de6954982cf4f03a78f820dc8a35d4e3134
robnyman <robnyman@4dc669ec-2940-0410-9f04-cfe5fb82e8a9>
2008-03-10 09:10:41 +0000

    Added de-facto default header that most other JavaScript libraries use

--- DOMAssistantComplete.js
+++ DOMAssistantComplete.js
@@ -3,7 +3,7 @@ var DOMAssistant = function () {
 	var HTMLArray = function () {
 		// Constructor
 	};
-	var isIE = /*@cc_on!@*/false;
+	var isIE = false;
 	return {
 		allMethods : [],
 		publicMethods : [
@@ -1121,7 +1121,8 @@ DOMAssistant.AJAX = function () {
 					var headers = ajaxObj.headers;
 					var addToContent = ajaxObj.addToContent;
 					XMLHttp.open(method, url, true);
-					XMLHttp.setRequestHeader("AJAX", "true");				
+					XMLHttp.setRequestHeader("AJAX", "true");
+					XMLHttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
 					if (method === "POST") {
 						var contentLength = (params)? params.length : 0;
 						XMLHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
