3ec4498c09069b5bcbea50839f38d7f80b1c4e41
robnyman <robnyman@4dc669ec-2940-0410-9f04-cfe5fb82e8a9>
2008-03-12 10:08:31 +0000

    Improved improved Unicode support :-)

--- trunk/modules/DOMAssistant.js
+++ trunk/modules/DOMAssistant.js
@@ -104,7 +104,7 @@ var DOMAssistant = function () {
 				var arg = arguments[0];
 				if (typeof arg === "string") {
 					arg = arg.replace(/^[^#]*(#)/, "$1");
-					if (/^#[\w\u0128-\uFFFF\u00C0-\u00FF\u0100-\u0127\-\_]+$/.test(arg)) {
+					if (/^#[\w\u00C0-\uFFFF\-\_]+$/.test(arg)) {
 						var idMatch = DOMAssistant.$$(arg.substr(1), false);
 						if (idMatch) {
 							elm.push(idMatch);
@@ -156,7 +156,7 @@ var DOMAssistant = function () {
 					var cssRules = cssRule.replace(/\s*(,)\s*/g, "$1").split(",");
 					var elm = new HTMLArray();
 					var currentRule, identical, cssSelectors, xPathExpression, cssSelector, splitRule, nextTag, followingElm;
-					var cssSelectorRegExp =  /^(\w+)?(#[\w\u0128-\uFFFF\u00C0-\u00FF\u0100-\u0127\-\_]+|(\*))?((\.[\w\u0128-\uFFFF\u00C0-\u00FF\u0100-\u0127\-_]+)*)?((\[\w+(\^|\$|\*)?=?[\w\u0128-\uFFFF\u00C0-\u00FF\u0100-\u0127\-\_]+\]+)*)?(((:\w+[\w\-]*)(\((odd|even|\d+n?((\+|\-)\d+)?|\w+|((\w*\.[\w\-_]+)*)?|(\[#?\w+(\^|\$|\*)?=?[\w\-\_]+\]+))\))?)*)?(>|\+|~)?/;
+					var cssSelectorRegExp =  /^(\w+)?(#[\w\u00C0-\uFFFF\-\_]+|(\*))?((\.[\w\u00C0-\uFFFF\-_]+)*)?((\[\w+(\^|\$|\*)?=?[\w\u00C0-\uFFFF\-\_]+\]+)*)?(((:\w+[\w\-]*)(\((odd|even|\d+n?((\+|\-)\d+)?|\w+|((\w*\.[\w\-_]+)*)?|(\[#?\w+(\^|\$|\*)?=?[\w\-\_]+\]+))\))?)*)?(>|\+|~)?/;
 					for (var i=0, il=cssRules.length; i<il; i++) {
 						currentRule = cssRules[i];
 						if (i > 0) {
@@ -204,10 +204,10 @@ var DOMAssistant = function () {
 								xPathExpression += "[@id = '" + splitRule.id.replace(/^#/, "") + "']";
 							}
 							if (splitRule.allClasses) {
-								xPathExpression += splitRule.allClasses.replace(/\.([\w\u0128-\uFFFF\u00C0-\u00FF\u0100-\u0127\-_]+)/g, "[contains(concat(' ', @class, ' '), ' $1 ')]");
+								xPathExpression += splitRule.allClasses.replace(/\.([\w\u00C0-\uFFFF\-_]+)/g, "[contains(concat(' ', @class, ' '), ' $1 ')]");
 							}
 							if (splitRule.allAttr) {
-								xPathExpression += splitRule.allAttr.replace(/(\w+)(\^|\$|\*)?=?([\w\u0128-\uFFFF\u00C0-\u00FF\u0100-\u0127\-_]+)?/g, function (match, p1, p2, p3, p4) {
+								xPathExpression += splitRule.allAttr.replace(/(\w+)(\^|\$|\*)?=?([\w\u00C0-\uFFFF\-_]+)?/g, function (match, p1, p2, p3, p4) {
 									var regExpReturn = match;
 									switch (p2) {
 										case "^":
@@ -370,7 +370,7 @@ var DOMAssistant = function () {
 					var matchingElms = new HTMLArray();
 					var prevParents, currentRule, identical, cssSelectors, childOrSiblingRef, nextTag, nextSelector, nextRegExp, refSeparator, refPrevElm, nextSib, refPrevElmFound, current, previous, prevParent, addElm, firstChild, lastChild, parentTagsByType, matchingChild, childrenNodes, childNodes;
 					var childOrSiblingRefRegExp = /^(>|\+|~)$/;
-					var cssSelectorRegExp = /^(\w+)?(#[\w\u0128-\uFFFF\u00C0-\u00FF\u0100-\u0127\-\_]+|(\*))?((\.[\w\u0128-\uFFFF\u00C0-\u00FF\u0100-\u0127\-_]+)*)?((\[\w+(\^|\$|\*)?=?[\w\u0128-\uFFFF\u00C0-\u00FF\u0100-\u0127\-\_]+\]+)*)?(((:\w+[\w\-]*)(\((odd|even|\d*n?((\+|\-)\d+)?|\w+|((\w*\.[\w\-_]+)*)?|(\[#?\w+(\^|\$|\*)?=?[\w\-\_]+\]+))\))?)*)?/;
+					var cssSelectorRegExp = /^(\w+)?(#[\w\u00C0-\uFFFF\-\_]+|(\*))?((\.[\w\u00C0-\uFFFF\-_]+)*)?((\[\w+(\^|\$|\*)?=?[\w\u00C0-\uFFFF\-\_]+\]+)*)?(((:\w+[\w\-]*)(\((odd|even|\d*n?((\+|\-)\d+)?|\w+|((\w*\.[\w\-_]+)*)?|(\[#?\w+(\^|\$|\*)?=?[\w\-\_]+\]+))\))?)*)?/;
 					var matchedObjects;
 					function clearAdded() {
 						for (var n=0, nl=prevElm.length; n<nl; n++) {
@@ -536,7 +536,7 @@ var DOMAssistant = function () {
 								if (splitRule.allAttr) {
 									splitRule.allAttr = splitRule.allAttr.replace(/(\])(\[)/, "$1 $2").split(" ");
 									var regExpAttributes = [];
-									var attributeMatchRegExp = /(\w+)(\^|\$|\*)?=?([\w\u0128-\uFFFF\u00C0-\u00FF\u0100-\u0127\-_]+)?/;
+									var attributeMatchRegExp = /(\w+)(\^|\$|\*)?=?([\w\u00C0-\uFFFF\-_]+)?/;
 									for (var sp=0, spl=splitRule.allAttr.length, attributeMatch, attribute, attributeValue, attrVal, tag, substrMatchSelector; sp<spl; sp++) {
 										attributeMatch = attributeMatchRegExp.exec(splitRule.allAttr[sp]);
 										attributeValue = attributeMatch[3] || null;
