b29329577a946893da15d8144e706edb9a651310
robnyman <robnyman@4dc669ec-2940-0410-9f04-cfe5fb82e8a9>
2008-05-27 19:26:15 +0000

    2.7.1 release

---
+++ branches/2.7.1/modules/DOMAssistantAJAX.js
@@ -0,0 +1,164 @@
+// Developed by Robert Nyman/DOMAssistant team, code/licensing: http://code.google.com/p/domassistant/, documentation: http://www.domassistant.com/documentation
+/*extern DOMAssistant */
+DOMAssistant.AJAX = function () {
+	var globalXMLHttp = null;
+	var readyState = 0;
+	var status = -1;
+	var statusText = "";
+	var createAjaxObj = function (url, method, callback, addToContent) {
+		var params = null;
+		if (/POST/i.test(method)) {
+			url = url.split("?");
+			params = url[1];
+			url = url[0];
+		}
+		return {
+			url: url,
+			method : method,
+			callback : callback,
+			params : params,
+			headers : {},
+			responseType : "text",
+			addToContent : addToContent || false
+		};
+	};
+	return {
+		publicMethods : [
+			"ajax",
+			"get",
+			"post",
+			"load",
+			"replaceWithAJAXContent"
+		],
+		
+		initRequest : function () {
+			var XMLHttp = null;
+			if (typeof XMLHttpRequest !== "undefined") {
+				XMLHttp = new XMLHttpRequest();
+				DOMAssistant.AJAX.initRequest = function () {
+					return new XMLHttpRequest();
+				};
+			}
+			else if (typeof window.ActiveXObject !== "undefined") {
+				var XMLHttpMS = ["Msxml2.XMLHTTP.6.0", "Msxml2.XMLHTTP.3.0", "Msxml2.XMLHTTP", "Microsoft.XMLHTTP"];
+				for (var i=0; i<XMLHttpMS.length; i++) {
+					try	{
+						XMLHttp = new window.ActiveXObject(XMLHttpMS[i]);
+						DOMAssistant.AJAX.initRequest = function () {
+							return new window.ActiveXObject(XMLHttpMS[i]);
+						};
+						break;
+					}
+					catch (e) {
+						XMLHttp = null;
+					}
+				}
+			}
+			return XMLHttp;
+		},
+		
+		ajax : function (ajaxObj) {
+			if (!ajaxObj.noParse && ajaxObj.url && /\?/.test(ajaxObj.url) && ajaxObj.method && /POST/i.test(ajaxObj.method)) {
+				var url = ajaxObj.url.split("?");
+				ajaxObj.url = url[0];
+				ajaxObj.params = url[1] + ((url[1].length > 0 && ajaxObj.params)? ("&" + ajaxObj.params) : "");
+			}
+			return DOMAssistant.AJAX.makeCall.call(this, ajaxObj);
+		},
+	
+		get : function (url, callback, addToContent) {
+			var ajaxObj = createAjaxObj(url, "GET", callback, addToContent);
+			return DOMAssistant.AJAX.makeCall.call(this, ajaxObj);
+		},
+		
+		post : function (url, callback) {
+			var ajaxObj = createAjaxObj(url, "POST", callback);
+			return DOMAssistant.AJAX.makeCall.call(this, ajaxObj);
+		},
+		
+		load : function (url, addToContent) {
+			DOMAssistant.AJAX.get.call(this, url, DOMAssistant.AJAX.replaceWithAJAXContent, addToContent);
+		},
+		
+		makeCall : function (ajaxObj) {
+			var XMLHttp = DOMAssistant.AJAX.initRequest();
+			if (XMLHttp) {
+				globalXMLHttp = XMLHttp;
+				var ajaxCall = function (elm) {
+					var url = ajaxObj.url;
+					var method = ajaxObj.method || "GET";
+					var callback = ajaxObj.callback;
+					var params = ajaxObj.params;
+					var headers = ajaxObj.headers;
+					var responseType = ajaxObj.responseType || "text";
+					var addToContent = ajaxObj.addToContent;
+					XMLHttp.open(method, url, true);
+					XMLHttp.setRequestHeader("AJAX", "true");
+					XMLHttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+					if (method === "POST") {
+						var contentLength = params? params.length : 0;
+						XMLHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+						XMLHttp.setRequestHeader("Content-length", contentLength);
+						if (XMLHttp.overrideMimeType) {
+							XMLHttp.setRequestHeader("Connection", "close");
+						}
+					}
+					for (var i in headers) {
+						if (typeof i === "string") {
+							XMLHttp.setRequestHeader(i, headers[i]);
+						}
+					}
+					if (typeof callback === "function") {
+						XMLHttp.onreadystatechange = function () {
+							if (XMLHttp.readyState === 4) {
+								var response = (/xml/i.test(responseType))? XMLHttp.responseXML : XMLHttp.responseText;
+								callback.call(elm, response, addToContent);
+								readyState = 4;
+								status = XMLHttp.status;
+								statusText = XMLHttp.statusText;
+								globalXMLHttp = null;
+								XMLHttp = null;
+							}
+						};
+					}
+					XMLHttp.send(params);
+				}(this);				
+			}
+			return this;
+		},
+		
+		replaceWithAJAXContent : function (content, add) {
+			if (add) {
+				this.innerHTML += content;
+			}
+			else {
+				var elms = this.all || this.getElementsByTagName("*");
+				for (var i=0, elm, attr; (elm=elms[i]); i++) {
+					attr = elm.attributes;
+					if (attr) {
+						for (var j=0, jl=attr.length, att; j<jl; j++) {
+							att = attr[j].nodeName.toLowerCase();
+							if (typeof elm[att] === "function") {
+								elm[att] = null;
+							}
+						}
+					}
+				}
+				this.innerHTML = content;
+			}
+		},
+		
+		getReadyState : function () {
+			return (globalXMLHttp && typeof globalXMLHttp.readyState !== "undefined")? globalXMLHttp.readyState : readyState;
+		},
+		
+		getStatus : function () {
+			return status;
+		},
+		
+		getStatusText : function () {
+			return statusText;
+		}
+	};
+}();
+DOMAssistant.attach(DOMAssistant.AJAX);
---
+++ branches/2.7.1/modules/DOMAssistantLoad.js
@@ -0,0 +1,80 @@
+// Developed by Robert Nyman/DOMAssistant team, code/licensing: http://code.google.com/p/domassistant/, documentation: http://www.domassistant.com/documentation. Module inspiration by Dean Edwards, Matthias Miller, and John Resig: http://dean.edwards.name/weblog/2006/06/again/
+/*extern DOMAssistant */
+DOMAssistant.DOMLoad = function () {
+	var DOMLoaded = false;
+	var DOMLoadTimer = null;
+	var functionsToCall = [];
+	var addedStrings = {};
+	var errorHandling = null;
+	var execFunctions = function () {
+		for (var i=0, il=functionsToCall.length; i<il; i++) {
+			try {
+				functionsToCall[i]();
+			}
+			catch (e) {
+				if (errorHandling && typeof errorHandling === "function") {
+					errorHandling(e);
+				}
+			}
+		}
+		functionsToCall = [];
+	};
+	var DOMHasLoaded = function () {
+		if (DOMLoaded) {
+			return;
+		}
+		DOMLoaded = true;
+		execFunctions();
+	};
+	/* Internet Explorer */
+	/*@cc_on
+	@if (@_win32 || @_win64)
+		if (document.getElementById) {
+			document.write("<script id=\"ieScriptLoad\" defer src=\"//:\"><\/script>");
+			document.getElementById("ieScriptLoad").onreadystatechange = function() {
+				if (this.readyState === "complete") {
+					DOMHasLoaded();
+				}
+			};
+		}
+	@end @*/
+	/* Mozilla/Opera 9 */
+	if (document.addEventListener) {
+		document.addEventListener("DOMContentLoaded", DOMHasLoaded, false);
+	}
+	/* Safari, iCab, Konqueror */
+	if (/KHTML|WebKit|iCab/i.test(navigator.userAgent)) {
+		DOMLoadTimer = setInterval(function () {
+			if (/loaded|complete/i.test(document.readyState)) {
+				DOMHasLoaded();
+				clearInterval(DOMLoadTimer);
+			}
+		}, 10);
+	}
+	/* Other web browsers */
+	window.onload = DOMHasLoaded;
+	
+	return {
+		DOMReady : function () {
+			for (var i=0, il=arguments.length, funcRef; i<il; i++) {
+				funcRef = arguments[i];
+				if (!funcRef.DOMReady && !addedStrings[funcRef]) {
+					if (typeof funcRef === "string") {
+						addedStrings[funcRef] = true;
+						funcRef = new Function(funcRef);
+					}
+					funcRef.DOMReady = true;
+					functionsToCall.push(funcRef);
+				}
+			}
+			if (DOMLoaded) {
+				execFunctions();
+			}
+		},
+		
+		setErrorHandling : function (funcRef) {
+			errorHandling = funcRef;
+		}
+	};
+}();
+DOMAssistant.DOMReady = DOMAssistant.DOMLoad.DOMReady;
