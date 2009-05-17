c4c46b2a922c1fcadc870be065af8c6377bf91f8
chenghong <chenghong@4dc669ec-2940-0410-9f04-cfe5fb82e8a9>
2009-05-17 08:55:36 +0000

    Reduced code in Xpath engine

--- trunk/DOMAssistantComplete.js
+++ trunk/DOMAssistantComplete.js
@@ -665,27 +665,10 @@ var DOMAssistant = function () {
 				}
 				function pseudoToXPath (tag, pseudoClass, pseudoValue) {
 					tag = /\-child$/.test(pseudoClass)? "*" : tag;
-					var xpath = "", pseudo = pseudoClass.split("-"), recur;
+					var pseudo = pseudoClass.split("-"), position = ((pseudo[1] === "last")? "(count(following-sibling::" : "(count(preceding-sibling::") + tag + ") + 1)", recur, hash;
 					switch (pseudo[0]) {
-						case "nth":
-							if (!/^n$/.test(pseudoValue)) {
-								var position = ((pseudo[1] === "last")? "(count(following-sibling::" : "(count(preceding-sibling::") + tag + ") + 1)";
-								if ((sequence = DOMAssistant.getSequence(pseudoValue))) {
-									xpath = (sequence.start === sequence.max)?
-										position + " = " + sequence.start :
-										position + " mod " + sequence.add + " = " + sequence.modVal + ((sequence.start > 1)? " and " + position + " >= " + sequence.start : "") + ((sequence.max > 0)? " and " + position + " <= " + sequence.max: "");
-								}
-							}
-							break;
-						case "not":
-							var notSelector = (recur = regex.pseudo.exec(pseudoValue))?
-								pseudoToXPath(tag, recur[1]? recur[1].toLowerCase() : null, recur[3] || null) :
-								pseudoValue.replace(regex.id, "[id=$1]")
-									.replace(regex.tag, "self::$1")
-									.replace(regex.classes, "contains(concat(\" \", @class, \" \"), \" $1 \")")
-									.replace(regex.attribs, attrToXPath);
-							xpath = "not(" + notSelector + ")";
-							break;
+						case "nth": return (pseudoValue !== "n" && (sequence = DOMAssistant.getSequence(pseudoValue)))? ((sequence.start === sequence.max)? position + " = " + sequence.start : position + " mod " + sequence.add + " = " + sequence.modVal + ((sequence.start > 1)? " and " + position + " >= " + sequence.start : "") + ((sequence.max > 0)? " and " + position + " <= " + sequence.max: "")) : "";
+						case "not": return "not(" + ((recur = regex.pseudo.exec(pseudoValue))? pseudoToXPath(tag, recur[1]? recur[1].toLowerCase() : null, recur[3] || null) : pseudoValue.replace(regex.id, "[id=$1]").replace(regex.tag, "self::$1").replace(regex.classes, "contains(concat(\" \", @class, \" \"), \" $1 \")").replace(regex.attribs, attrToXPath)) + ")";
 						case "first": return "not(preceding-sibling::" + tag + ")";
 						case "last": return "not(following-sibling::" + tag + ")";
 						case "only": return "not(preceding-sibling::" + tag + " or following-sibling::" + tag + ")";
@@ -693,7 +676,7 @@ var DOMAssistant = function () {
 						case "contains": return "contains(., \"" + pseudoValue.replace(regex.quoted, "$1") + "\")";
 						case "enabled": return "not(@disabled) and not(@type=\"hidden\")";
 						case "disabled": return "@disabled";
-						case "target": var hash = document.location.hash.slice(1); return "@name=\"" + hash + "\" or @id=\"" + hash + "\"";
+						case "target": return "@name=\"" + (hash = document.location.hash.slice(1)) + "\" or @id=\"" + hash + "\"";
 						default: return "@" + pseudoClass + "=\"" + pseudoValue + "\"";
 					}
 					return xpath;
