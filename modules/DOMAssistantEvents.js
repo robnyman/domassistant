e9f45affb6c3acb0b3ccc43fc4cdb489c10b8586
chenghong@gmail.com <chenghong@gmail.com@4dc669ec-2940-0410-9f04-cfe5fb82e8a9>
2011-11-30 15:08:38 +0000

    relayEvent was broken for mouseenter and mouseleave

--- modules/DOMAssistantEvents.js
+++ modules/DOMAssistantEvents.js
@@ -5,7 +5,7 @@ DOMAssistant.Events = function () {
 		key = "_events",
 		w3cMode = !!document.addEventListener,
 		useCapture = { focus: true, blur: true },
-		translate = DOMAssistant.isIE? { focus: "activate", blur: "deactivate" } : { mouseenter: "mouseover", mouseleave: "mouseout" },
+		translate = DOMAssistant.isIE? { focus: "activate", blur: "deactivate", mouseenter: "mouseover", mouseleave: "mouseout" } : { mouseenter: "mouseover", mouseleave: "mouseout" },
 		regex = {
 			special: /^submit|reset|change|select$/i,
 			mouseenterleave: /^mouse(enter|leave)$/i,
@@ -149,7 +149,7 @@ DOMAssistant.Events = function () {
 							if (relatedTarg && regex.mouseenterleave.test(oevt)) {
 								if (data.relay) {
 									var elms = data.elms || (data.elms = eventColl[i].data[type].elms = this.cssSelect(data.selector));
-									if (elms.indexOf(targ) < 0 || !DOMAssistant.hasChild.call(relatedTarg, targ)) { continue; }
+									if (elms.indexOf(targ) < 0 || DOMAssistant.hasChild.call(targ, relatedTarg)) { continue; }
 								}
 								else if (this === relatedTarg || this.hasChild(relatedTarg)) { continue; }
 							}
